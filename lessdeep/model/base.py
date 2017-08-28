3ede6ff7d6a78ad2022143a43ac8f0d472cfa3da
7kbird <7kbird@gmail.com>
2017-08-28 08:08:37 +0800

    a seperate vgg16 model is created, not finished

--- lessdeep/model/base.py
+++ lessdeep/model/base.py
@@ -1,7 +1,21 @@
 import os
+import keras
 
 __default_cache_root=os.path.realpath(os.path.join(os.path.dirname(__file__), '_cache'))
 
 
 def cache_dir(name):
     return os.path.join(__default_cache_root, name)
+
+
+def get_batches(path_dir, generator, shuffle=True, batch_size=8, class_mode='categorial', **kwargs):
+    '''Load batches from directory and return a batch generator
+    '''
+    if type(generator) is keras.preprocessing.image.ImageDataGenerator:
+        batches = generator.flow_from_directory(directory=path_dir, shuffle=shuffle,
+                                                batch_size=32, **kwargs)
+        if batches.samples < batch_size:
+            batches.batch_size = batches.samples  # keras raise error when batch_size is larger than total samples
+        return batches
+    else:
+        raise NotImplemented('Generator %s is not supported' % generator)
--- lessdeep/model/vgg16.py
+++ lessdeep/model/vgg16.py
@@ -5,3 +5,44 @@ Created on Sat Aug 26 16:16:42 2017
 @author: doctor
 """
 
+import numpy as np
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from . import base
+
+
+class Vgg16(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_batches(self, path_dir, generator=None, shuffle=True, batch_size=8, class_mode='categorial', **kwargs):
+        '''Setup preprocessing for vgg16
+
+        example:
+
+        '''
+        if not generator:
+            generator = ImageDataGenerator()
+
+        # To make best use of cpu, preprocessing is added in batch loading
+        def _norm_img(img):
+            # input_shape=(3,224,224)  of (depth, height, width)
+            # Mean of each channel as provided by VGG researchers
+            vgg_mean = np.array([123.68, 116.779, 103.939]).reshape((3, 1, 1))
+            return (img - vgg_mean)[::-1]  # convert rgb to bgr
+
+        # make sure the data is loaded as (depth, height, width)
+        generator.data_format = 'channels_first'
+
+        # add normalization as preprocessing
+        if generator.preprocessing_function:
+            f = generator.preprocessing_function
+            generator.preprocessing_function = lambda x, f_norm=_norm_img, f_old=f: f_norm(f_old(x))
+        else:
+            generator.preprocessing_function = _norm_img
+
+        # NOTE: target_size must be (224, 224) for the pretrained model so that all image are resized
+        return base.get_batches(path_dir, generator, shuffle=shuffle, batch_size=batch_size, class_mode=class_mode,
+                                target_size=(224, 224), **kwargs)
+
