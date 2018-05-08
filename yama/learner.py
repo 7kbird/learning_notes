5455743d413b3e2af693388ea20bf991ecd8877a
7kbird <7kbird@gmail.com>
2018-05-08 18:10:11 +0800

    try add Cyclic LR(clone from torch pull request)

---
+++ yama/learner.py
@@ -0,0 +1,11 @@
+
+
+class Learner(object):
+    def __init__(self):
+        pass
+
+    def find_lr(self, start_lr=1e-5, end_lr=10):
+        pass
+
+    # def find_batch_size(self, start_size=4):
+    #     pass
